afdddcc0464e62611ce372c93e8d7e4239a4102a
Lucas Campos - CamposTech <53227451+campostech@users.noreply.github.com>
2022-09-09 16:28:18 -0300

    fix hasAttr method

--- uwp.js
+++ uwp.js
@@ -1,16 +1,17 @@
 $(function() {
 
-    function hasAttr(attr) {
-        return (typeof $(this).attr(attr) !== 'undefined' && $(this).attr(attr) !== false);
+    function hasAttr(element, attr) {
+        return (typeof $(element).attr(attr) !== 'undefined' && $(element).attr(attr) !== false);
     }
 
     $('img[uwp]').each(function() {
-        console.log($(this).attr("uwp"));
         let uwp_id = `uwp_${$(this).attr("uwp")}`;
         let file_uwp_id = `file_${uwp_id}`;
         let b64_uwp_id = `b64_${uwp_id}`;
-        let required = hasAttr("required") ? `required=${$(this).attr("required")}` : "";
-        let maxSize = hasAttr("maxsize") ? $(this).attr("maxsize") : 5120;
+        console.log(hasAttr(this, "required"), hasAttr(this, "maxsize"));
+
+        let required = hasAttr(this, "required") ? `required=${$(this).attr("required")}` : "";
+        let maxSize = hasAttr(this, "maxsize") ? $(this).attr("maxsize") : 5120;
         $(this).attr("id", uwp_id);
         $(this).attr("onClick", `$("#${file_uwp_id}").click()`);
         let inputB64 = `<input ${required} type='text' class='uwp_input' id='${b64_uwp_id}' name='${$(this).attr("uwp")}' />`;
@@ -24,6 +25,7 @@ $(function() {
         if (!this.files || !this.files[0]) return;
         let maxSize = parseFloat($(this).attr('uwp_maxsize'));
         let fileSize = this.files[0].size / 1024;
+
         if (fileSize > maxSize) {
             alert(`File is too big! The maximum size is ${maxSize}kb`);
             this.value = "";
